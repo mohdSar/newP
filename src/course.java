8103a97589286de93a9c67fa5e4733206fda39e2
Mohammad Khaled Mohammad Alsarasra <201706944@NATIVITY.BETHLEHEM.EDU>
2018-05-04 15:12:37 +0300

    hahaha

--- src/course.java
+++ src/course.java
@@ -1,4 +1,16 @@
+import java.util.*;
 
 public class course {
+String code ; 
+String name;
+ArrayList<Double> grades = new ArrayList<>(); 
 
+course(){
+	
+}
+public course(String code, String name , ArrayList grades) {
+	this.name = name;
+	this.code = code;
+	this.grades = grades;
+}
 }
